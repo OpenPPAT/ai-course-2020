b5a1aaf9d0c9dcdaaf0d52e60d267a830d552091
ray0727 <rayhuang100@gmail.com>
2020-10-15 04:09:05 +0800

    fix environment path

--- 1022-Locobot-astar/environment.sh
+++ 1022-Locobot-astar/environment.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 source /opt/ros/melodic/setup.bash
-source ~/ncsist_threat_processing/catkin_ws/devel/setup.bash
+source ~/ai-course-2020/1022-Locobot-astar/catkin_ws/devel/setup.bash
 
-echo "ncsist_threat_processing environment"
+#echo "ncsist_threat_processing environment"
